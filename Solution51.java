4318218bc08359314851b516204682787cc86ce4
SakshiKumbhar <sakshikumbhar@orchidengg.ac.in>
2026-07-11 20:05:45 +0530

    Add files via upload

---
+++ Solution51.java
@@ -0,0 +1,55 @@
+/*
+5. Longest Palindromic Substring
+Given a string s, return the longest palindromic substring in s.
+
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ 
+ */
+class Solution51 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2)
+            return s;
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Odd length palindrome
+            int len1 = expand(s, i, i);
+
+            // Even length palindrome
+            int len2 = expand(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right) {
+
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
