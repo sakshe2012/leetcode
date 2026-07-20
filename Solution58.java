06dcae5e6a82a1bd36e85a1f81a66c919c1cb8e4
SakshiKumbhar <sakshikumbhar@orchidengg.ac.in>
2026-07-20 20:25:47 +0530

    Add files via upload

---
+++ Solution58.java
@@ -0,0 +1,57 @@
+/*
+94. Binary Tree Inorder Traversal
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+*/
+
+class Solution58{
+    public List<Integer> inorderTraversal(TreeNode root) {
+List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, result);   
+        result.add(node.val);         
+        inorder(node.right, result);  
+    }
+}
